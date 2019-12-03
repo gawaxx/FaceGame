a4a87efe6ff7862ae43a902baea23be0a479ee4e
gortron <gordon.lanza@gmail.com>
2019-12-03 13:24:52 +0000

    wip logic for isMouthOpen

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 var video = document.querySelector("#videoElement");
-
+window.addEventListener("click", () => {
+  console.log(event.clientY);
+});
 Promise.all([
   faceapi.nets.tinyFaceDetector.loadFromUri("../src/models"),
   faceapi.nets.faceLandmark68Net.loadFromUri("../src/models")
@@ -28,14 +30,65 @@ video.addEventListener("play", () => {
 
     const landmarks = await faceapi.detectFaceLandmarks(video);
     const mouth = landmarks.getMouth();
-    mouthCoordinates(mouth);
+    // mouthCoordinates(mouth);
+    mouthIsOpen(mouth);
+    // console.log(mouthIsOpen(mouth));
     // console.log(`X: ${mouth0._x}, Y: ${mouth0._y}`);
-  }, 1);
+  }, 1000);
 });
 
 function mouthCoordinates(mouth) {
-  let innerLip = mouth.slice(-8);
-  innerLip.forEach(landmark =>
-    console.log(`X: ${landmark._x}, Y: ${landmark._y}`)
-  );
+  // let innerLip = mouth.slice(-8);
+  // innerLip.forEach(landmark =>
+  //   console.log(`X: ${landmark._x}, Y: ${landmark._y}`)
+  // );
+  console.log(mouth);
 }
+
+function mouthIsOpen(mouth) {
+  console.log(mouth);
+  let outerLipTop = mouth[9].y;
+  let innerLipTop = mouth[18].y;
+  let innerLipBottom = mouth[14].y;
+  let outerLipBottom = mouth[3].y;
+  // console.log(`outerLipTop: ${outerLipTop}`);
+  // console.log(`innerLipTop: ${innerLipTop}`);
+  // console.log(`innerLipBottom: ${innerLipBottom}`);
+  // console.log(`outerLipBottom: ${outerLipBottom}`);
+  // console.log("---------------------------");
+
+  // let topLipHeight = outerLipTop - innerLipTop;
+  // let bottomLipHeight = innerLipBottom - outerLipBottom;
+  // let totalLipHeight = topLipHeight + bottomLipHeight;
+
+  let lipHeight = outerLipTop - outerLipBottom;
+  let mouthHeight = innerLipTop - innerLipBottom;
+  // let lipSize = lipHeight - mouthHeight;
+  let mouthToLipRatio = mouthHeight / lipHeight; // produces % of height which is lip
+  let mouthOpen = mouthToLipRatio > 0.8;
+  // let mouthOpen = mouthHeight > totalLipHeight;
+  console.log(`lipHeight: ${lipHeight} // mouthHeight: ${mouthHeight}`);
+  console.log(`ratio: ${mouthToLipRatio} // mouthOpen: ${mouthOpen}`);
+  if (mouthOpen) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+// Example closed:
+// outerLipTop: 298.1619358062744
+// innerLipTop: 282.05471992492676
+// innerLipBottom: 236.75374031066895
+// outerLipBottom: 223.34667205810547
+// lipHeight: 75
+// mouthHeight: 46
+//
+//
+// Example open:
+// outerLipTop: 317.14799880981445
+// innerLipTop: 314.7025966644287
+// innerLipBottom: 238.69019508361816
+// outerLipBottom: 231.71459197998047
+// lipHeight: 86
+// mouthHeight: 76
