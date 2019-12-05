6b7c2fa759e61eb100e4548cf90582aab8d84156
gawaxx <jules.blanc@unccub.com>
2019-12-05 10:06:29 +0000

    Allow for cors request

--- back-end-API/app/controllers/score_boards_controller.rb
+++ back-end-API/app/controllers/score_boards_controller.rb
@@ -10,11 +10,11 @@ class ScoreBoardsController < ApplicationController
         render json: { id: scoreboard.id, name: scoreboard.user.name, count: scoreboard.count }
     end
 
-    def create
-        new_scoreboard = ScoreBoard.new 
-        new_scoreboard.user_id = params[:user_id]
-        new_scoreboard.count = params[:count]
-        render json: new_scoreboard
-    end 
+    # def create
+    #     new_scoreboard = ScoreBoard.new 
+    #     new_scoreboard.user_id = params[:user_id]
+    #     new_scoreboard.count = params[:count]
+    #     render json: new_scoreboard
+    # end 
 
 end
--- back-end-API/config/routes.rb
+++ back-end-API/config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   get '/' => 'application#home'
-  get 'score_boards/create', to: 'score_boards#create'
+  # get 'score_boards/create', to: 'score_boards#create'
 
   
 end
